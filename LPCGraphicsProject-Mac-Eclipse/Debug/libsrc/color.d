3a2e5ee849edbb5d74235b52de31d68fc6ce1cc5
Colin Schatz <colin.schatz@gmail.com>
2018-04-10 11:35:00 -0700

    Cleaned Mac Eclipse project

--- LPCGraphicsProject-Mac-Eclipse/Debug/libsrc/color.d
+++
@@ -1,7 +0,0 @@
-libsrc/color.o: ../libsrc/color.cpp \
-  /Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/color.h \
-  /Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/utility.h
-
-/Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/color.h:
-
-/Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/utility.h:
--- LPCGraphicsProject-Mac-Eclipse/Debug/libsrc/utility.d
+++
@@ -1,4 +0,0 @@
-libsrc/utility.o: ../libsrc/utility.cpp \
-  /Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/utility.h
-
-/Users/cschatz/Developer/LPCGraphics/LPCGraphicsProject-Mac-Eclipse/include/utility.h:
